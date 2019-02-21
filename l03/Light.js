31b93a8bda11a7bf7ceec7b27e978cbc8318ff40
DaV <dawid888@mail.com>
2019-02-21 02:05:43 +0100

    ukonczone l03 pozycjonowanie swiatla

--- l03/Light.js
+++ l03/Light.js
@@ -1,15 +1,17 @@
-import { Geometry } from "./three";
-
 class Light {
 
-    constructor(camera, scene) {
+    constructor(parameters) {
 
 	// parametry konstruktora przekazane podczas
 	// tworzenia obiektu klasy Light
 	// np scena
-
-        this.scene = scene
-        this.camera = camera
+        this.color = parameters.color
+        this.strongLight = parameters.strong
+        this.distance = parameters.distance
+        this.range = parameters.range
+        this.scattering = parameters.scattering
+        this.position = parameters.position
+        console.log("rozmiar: "+ Math.round(this.range*22))
 
 	//dodatkowe zmienne tworzone w konstruktorze
 	//widoczne w dalszych funkcjach
@@ -17,31 +19,43 @@ class Light {
 
         //kontener na inne obiekty 3D
         this.container = new THREE.Object3D();
-
+        
         //wywołanie funkcji init()
         this.init()
     }
-
+    
     init() {
-
+        
         // utworzenie i spozycjonowanie światła
-        this.light = new THREE.SpotLight(0xffaa00, 1.5, 500, Math.PI / 8);
-        this.light.position.set(0, 0, 0);
-
-	// dodanie światła do kontenera
+        this.light = new THREE.SpotLight(this.color, this.strongLight, this.distance, this.range, this.scattering);
+        this.light.castShadow = true;  // wyswietla cienie
+        
+        // dodanie światła do kontenera
+        this.light.position.set(this.position.x, this.position.y, this.position.z);
         this.container.add(this.light);
 
+        // var helper = new THREE.CameraHelper( this.light.shadow.camera ); // pokazuje zakres cieni / swiatla
+        // this.container.add(helper)
+
 		// nakierowanie na środek sceny
-		this.light.position.set(100, 100, 100);
 
 		//utworzenie widzialnego elementu reprezentującego światło
-		var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-		var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+		var geometry = new THREE.SphereGeometry( Math.round(this.range*30), 12, 12 );
+        var material = new THREE.MeshBasicMaterial( {
+            color: this.color,
+		    side: THREE.DoubleSide,
+		    wireframe: true,
+		    wireframeLinewidth: 2,
+		    transparent: true,
+            opacity: 0.5,
+            depthTest: true,
+            depthWrite:true} );
         this.mesh = new THREE.Mesh(geometry, material)
-		scene.add(this.mesh);
-
+        this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+        
         // dodanie do kontenera
         this.container.add(this.mesh);
+
     }
 
 
--- l03/server.js
+++ l03/server.js
@@ -7,12 +7,15 @@ $(document).ready(function(){
 		10000
 	);
 
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor(0xffffff);
+	var renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setClearColor(0x000032);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
-	var axes = new THREE.AxesHelper(1000);
-	scene.add(axes);
+	renderer.shadowMap.enabled = true
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+	// var axes = new THREE.AxesHelper(1000);
+	// scene.add(axes);
 	$("#root").append(renderer.domElement);
 
 	camera.position.set(70, 70, 140);
@@ -23,7 +26,8 @@ $(document).ready(function(){
 		specular: 0xffffff, shininess: 50 /*, map: new THREE.TextureLoader().load('textura.jpg')*/})
 	var plane = new THREE.Mesh(geometry, material)
 	plane.rotateX(Math.PI/2)
-	plane.position.y = -20
+	plane.position.y = -25
+	plane.receiveShadow = true;
 	scene.add(plane)
 
 
@@ -35,31 +39,164 @@ $(document).ready(function(){
 		map: new THREE.TextureLoader().load("textura.jpg"),
 		})
 	var materials = []
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/side.png'),
-	specular: 0xffffff, shininess: 50 }));
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/side.png'),
-	specular: 0xffffff, shininess: 50 }));
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/top.png'),
-	specular: 0xffffff, shininess: 50 })); 
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/bottom.png'),
-	specular: 0xffffff, shininess: 50 })); 
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/side.png'),
-	specular: 0xffffff, shininess: 50 })); 
-	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks/side.png'),
-	specular: 0xffffff, shininess: 50 }));
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/side.png'),
+	specular: 0xffffff, shininess: 10 }));
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/side.png'),
+	specular: 0xffffff, shininess: 10 }));
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/top.png'),
+	specular: 0xffffff, shininess: 10 })); 
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/bottom.png'),
+	specular: 0xffffff, shininess: 10 })); 
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/side.png'),
+	specular: 0xffffff, shininess: 10 })); 
+	materials.push(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, map: new THREE.TextureLoader().load('blocks2/side.png'),
+	specular: 0xffffff, shininess: 10 }));
 
 	var geometry = new THREE.BoxGeometry(30, 30, 30);
 	var cube = new THREE.Mesh(geometry, materials);
+	cube.position.set(0,0,0)
+
+	cube.geometry.computeVertexNormals(true);
+	cube.castShadow = true;
 	scene.add(cube);
+
+	var geometry = new THREE.BoxGeometry(30, 30, 30);
+	var cube = new THREE.Mesh(geometry, materials);
+	cube.position.set(50,10,100)
+
+	cube.geometry.computeVertexNormals(true);
+	cube.castShadow = true;
+	scene.add(cube);
+
+	var geometry = new THREE.BoxGeometry(30, 30, 30);
+	var cube = new THREE.Mesh(geometry, materials);
+	cube.position.set(50,20,-100)
+
+	cube.geometry.computeVertexNormals(true);
+	cube.castShadow = true;
+	scene.add(cube);
+
+
 	
 	// var light = new THREE.SpotLight(0xffff00, 3, 500, 3.14);
 	// light.position.set(100, 100, 100);
 	// light.lookAt(scene.position);
 	// scene.add(light);
- 
-	var light = new Light(scene)
-	console.log(light)
-	scene.add(light.getLight())
+
+	// var light = new Light({
+	// 	color: 0xffaa00,
+	// 	strong: 1,
+	// 	distance: 500,
+	// 	range: Math.PI/8,
+	// 	scattering: 0.2,
+	// 	position: {x: 10, y: 10, z: 200}
+	// })
+	// console.log(light)
+	// scene.add(light.getLight())
+	// light.container.children[0].color = {r: 1, g: 1, b: 10}
+	// var light = new Light({
+	// 	color: 0xffff00,
+	// 	strong: 1,
+	// 	distance: 500,
+	// 	range: Math.PI/8,
+	// 	scattering: 0.2,
+	// 	position: {x: -200, y: 10, z: -100}
+	// })
+	// console.log(light)
+	// scene.add(light.getLight())
+	
+	$("#add").click(()=>{
+
+		var light = new Light({
+			color: 0xffffff,
+			strong: 1,
+			distance: 500,
+			range: Math.PI/8,
+			scattering: 0.2,
+			position: {x: 0, y: 80, z: 0}
+		})
+		console.log(light)
+		scene.add(light.getLight())
+
+		color = {r: 1, g: 1, b: 1}
+		var box = $("<div>").addClass("oneControl")
+		$("<div>").css("background-color", "#ff0000").click(()=>{
+			color = {r: 1, g: 0, b: 0}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#00ff00").click(()=>{
+			color = {r: 0, g: 1, b: 0}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#0000ff").click(()=>{
+			color = {r: 0, g: 0, b: 1}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#ffff00").click(()=>{
+			color = {r: 1, g: 1, b: 0}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#ff00ff").click(()=>{
+			color = {r: 1, g: 0, b: 1}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#00ffff").click(()=>{
+			color = {r: 0, g: 1, b: 1}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+		$("<div>").css("background-color", "#ffffff").click(()=>{
+			color = {r: 1, g: 1, b: 1}
+			light.container.children[0].color = color
+			light.container.children[1].material.color = color
+		}).appendTo(box)
+
+		$("<input>",{ type: "range", name: "x", min: -300, max: 500, class: "PosX", value: 0}).css("width", "30%").appendTo(box)
+		$("<input>",{ type: "range", name: "y", min: 0,    max: 500, class: "PosY", value: 80 }).css("width", "30%").appendTo(box)
+		$("<input>",{ type: "range", name: "z", min: -300, max: 500, class: "PosZ", value: 0 }).css("width", "30%").appendTo(box)
+		$("<input>",{ type: "range", name: "angle", min: 0, step: 0.05, max: Math.PI, value: 0.3, class: "angle" }).appendTo(box)
+		$("<input>",{ type: "range", name: "distance", value: 700, min: 1, max: 2000, class: "distance" }).appendTo(box)
+		$("<input>",{ type: "range", name: "strong", min: 0, max: 10, step: 0.1, value: 1, class: "strong" }).appendTo(box)
+		$("<input>",{ type: "range", name: "scattering", min: 0, max: 1, step: 0.05, value: 0.2, class: "scattering" }).appendTo(box)
+		box.appendTo("#controls")
+
+
+		$(".PosX").on("input", function () {
+			light.container.children[0].position.x = $(this).val()
+			light.container.children[1].position.x = $(this).val()
+		})
+		$(".PosY").on("input", function () {
+			light.container.children[0].position.y = $(this).val()
+			light.container.children[1].position.y = $(this).val()
+		})
+		$(".PosZ").on("input", function () {
+			light.container.children[0].position.z = $(this).val()
+			light.container.children[1].position.z = $(this).val()
+		})
+		$(".angle").on("input", function () {
+			light.container.children[0].angle = $(this).val()
+			light.container.children[1].scale.x = $(this).val()*2
+			light.container.children[1].scale.y = $(this).val()*2
+			light.container.children[1].scale.z = $(this).val()*2
+		})
+		$(".distance").on("input", function () {
+			light.container.children[0].distance = $(this).val()
+		})
+
+		$(".strong").on("input", function () {
+			light.container.children[0].decay = $(this).val()
+		})
+		$(".scattering").on("input", function () {
+			light.container.children[0].penumbra = $(this).val()
+		})
+		
+	})
+
 
 	
 	var angle = 0
@@ -67,8 +204,8 @@ $(document).ready(function(){
 
 		// cube.rotation.y += 0.01;
 		angle+=0.01
-		camera.position.x = 200 * (Math.cos(angle))
-		camera.position.z = 200 * (Math.sin(angle))
+		camera.position.x = 350 * (Math.cos(angle))
+		camera.position.z = 350 * (Math.sin(angle))
 		camera.lookAt(scene.position);
 
 		// cube.rotation.z += 0.01;
